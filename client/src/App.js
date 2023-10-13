a3089208688de5645349628b5a6844c49db5f204
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-10-13 10:17:16 +0200

    Testing Deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            This was updated!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
